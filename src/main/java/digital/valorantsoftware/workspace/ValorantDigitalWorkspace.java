fda938ab641a36a6ce5e348303475306a12ac6e8
Software Shinobi <the.software.shinobi@gmail.com>
2024-02-22 05:11:44 -0500

    automated terminal push

--- src/main/java/digital/valorantsoftware/workspace/ValorantDigitalWorkspace.java
+++ src/main/java/digital/valorantsoftware/workspace/ValorantDigitalWorkspace.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ValorantDigitalWorkspace {
-  @Override
-  public void run(ApplicationArguments args) throws Exception {
-    System.in.read();
-  }
+
     public static void main(final String[] commandLineArguments) {
 
         SpringApplication.run(ValorantDigitalWorkspace.class, commandLineArguments);
